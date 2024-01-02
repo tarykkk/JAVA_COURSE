ee4e9d850d19ddf947710d949c5caee54e527231
tarykkk <max04tarasyuk@gmail.com>
2024-01-02 18:23:01 +0300

    Loaded lab_7

---
+++ src/main/java/com/university/lab_7/taryk/Exceptions/OutOfStockException.java
@@ -0,0 +1,7 @@
+package com.university.lab_7.taryk.Exceptions;
+
+public class OutOfStockException extends Exception{
+    public OutOfStockException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/university/lab_7/taryk/Main.java
@@ -0,0 +1,13 @@
+package com.university.lab_7.taryk;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        todo Part 1:
+         Class Product: id (integer), name(String), price(double), stock(int), toString()
+         Class User: id(integer), username(String), cart(Map<Product, Integer>
+         Class Order: id(integer), userId(integer), orderDetails(Map<Product,Integer>), totalPrice(double), totalSum
+        * */
+
+    }
+}
